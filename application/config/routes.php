4fa256e9207eee9521fdcfc762588822fe8337be
rizaldi <rizaldi354313@gmail.com>
2020-05-12 12:12:55 +0700

    depok

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-//JAKARTA
-$route['api/add/aqmdata/jakarta'] 	    = 'jakarta/aqmdata';
+//JADEBEK
+$route['api/add/aqmdata/depok']			= 'depok/aqmdata';
+$route['api/add/aqmdata/jakarta']		= 'jakarta/aqmdata';
 
 //SUKOHARJO
 $route['api/get/data/cemsrum']			= 'sukoharjo/cemsrumdata';
