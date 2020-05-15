1805049cce7403086f4ea02945687f428adb0127
rizaldi <rizaldi354313@gmail.com>
2020-05-15 10:34:37 +0700

    a

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,18 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 //SUMATERA
-$route['api/add/aqmdata/sumatera1']		= 'sumatera/aqmdata';
+$route['api/add/aqmdata/sumatera']		= 'sumatera/aqmdata';
 
 //SULAWESI
-$route['api/add/aqmdata/sulawesi1']		= 'sulawesi/aqmdata';
+$route['api/add/aqmdata/sulawesi']		= 'sulawesi/aqmdata';
 
 //KALIMANTAN
-$route['api/add/aqmdata/kalimantan1']	= 'kalimantan/aqmdata';
+$route['api/add/aqmdata/kalimantan']	= 'kalimantan/aqmdata';
 
 //JADEBEK
-$route['api/add/aqmdata/bekasi1']		= 'bekasi/aqmdata';
-$route['api/add/aqmdata/depok1']			= 'depok/aqmdata';
-$route['api/add/aqmdata/jakarta1']		= 'jakarta/aqmdata';
+$route['api/add/aqmdata/bekasi']		= 'bekasi/aqmdata';
+$route['api/add/aqmdata/depok']			= 'depok/aqmdata';
+$route['api/add/aqmdata/jakarta']		= 'jakarta/aqmdata';
 
 //SUKOHARJO
 $route['api/get/data/cemsrum']			= 'sukoharjo/cemsrumdata';
@@ -25,16 +25,16 @@ $route['api/add/cemsdata/sukoharjo']	= 'sukoharjo/cemsdata';
 $route['api/add/camsdata/sukoharjo']	= 'sukoharjo/camsdata';
 
 //CILEGON
-$route['api/get/data/pci1']				= 'cilegon/pcidata';
-$route['api/get/ispu/pci1']				= 'cilegon/pciispu';
-$route['api/get/data/simpangtiga1']		= 'cilegon/simpangtigadata';
-$route['api/get/ispu/simpangtiga1']		= 'cilegon/simpangtigaispu';
-$route['api/get/data/merak1']			= 'cilegon/merakdata';
-$route['api/get/ispu/merak1']			= 'cilegon/merakispu';
-$route['api/get/data/ciwandan1']			= 'cilegon/ciwandandata';
-$route['api/get/ispu/ciwandan1']			= 'cilegon/ciwandanispu';
+$route['api/get/data/pci']				= 'cilegon/pcidata';
+$route['api/get/ispu/pci']				= 'cilegon/pciispu';
+$route['api/get/data/simpangtiga']		= 'cilegon/simpangtigadata';
+$route['api/get/ispu/simpangtiga']		= 'cilegon/simpangtigaispu';
+$route['api/get/data/merak']			= 'cilegon/merakdata';
+$route['api/get/ispu/merak']			= 'cilegon/merakispu';
+$route['api/get/data/ciwandan']			= 'cilegon/ciwandandata';
+$route['api/get/ispu/ciwandan']			= 'cilegon/ciwandanispu';
 
-$route['api/add/aqmdata/cilegon1']		= 'cilegon/aqmdata';
+$route['api/add/aqmdata/cilegon']		= 'cilegon/aqmdata';
 
 $route['default_controller']			= 'api';
 $route['404_override']					= '';
